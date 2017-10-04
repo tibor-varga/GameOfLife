f5f3169f6906123bbc60dff4a6c6376a7ca85a87
buxi <buxi@laptop>
2017-10-04 15:16:44 +0200

    fixing broken test

--- src/test/java/com/buxi/gameoflife/LoadFileTest.java
+++ src/test/java/com/buxi/gameoflife/LoadFileTest.java
@@ -9,7 +9,7 @@ public class LoadFileTest {
 	@Test
 	public void testMatrixConstructor() throws URISyntaxException {
 		Matrix m = new Matrix(10, 10);
-		MatrixHelper.loadPatternIntoMatrix(m, "acorn.dat", new Coordinate(100, 100));
+		MatrixHelper.loadPatternIntoMatrix(m, "acorn.dat", new Coordinate(1, 1));
 
 		System.out.println(m);
 	}
