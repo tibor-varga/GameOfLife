73a582f7502b62428ff163a422d878eb60c0c95e
buxi <buxi@laptop>
2017-10-04 15:17:32 +0200

    fixing pattern calls

--- src/main/java/com/buxi/gameoflife/Starter.java
+++ src/main/java/com/buxi/gameoflife/Starter.java
@@ -21,9 +21,10 @@ public class Starter {
 
 		Matrix matrix = new Matrix(200, 200);
 		// MatrixHelper.initMatrixRandom(matrix);
-		// MatrixHelper.initWithAcorn(matrix,"acorn.dat", new Coordinate(100, 100));
-		// MatrixHelper.initWithDieHard(matrix,"diehard.dat", new Coordinate(100, 100));
-		// MatrixHelper.initWithRPentomino(matrix,"r-pentomino.dat", new Coordinate(100,
+		// MatrixHelper.initWithPattern(matrix, "acorn.dat", new Coordinate(100, 100));
+		// MatrixHelper.initWithPattern(matrix, "diehard.dat", new Coordinate(100,
+		// 100));
+		// MatrixHelper.initWithPattern(matrix, "r-pentomino.dat", new Coordinate(100,
 		// 100));
 		// MatrixHelper.initWithPattern(matrix, "pentadecathlon.dat", new Coordinate(80,
 		// 90));
@@ -36,6 +37,7 @@ public class Starter {
 		// 100));
 		// MatrixHelper.initWithPattern(matrix, "own.dat", new Coordinate(100, 100));
 		supernova(matrix);
+		// killPulsarWithFirework(matrix);
 		Screen frame = new Screen(5, 5, matrix);
 		frame.setSize(1700, 700);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
